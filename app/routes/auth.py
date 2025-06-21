095ad0abcf6122868bc4826c737bbc0355ee7d0f
CrafterJe <rauljs05123456789@gmail.com>
2025-06-21 17:45:32 -0600

    routes/auth.py Modified

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,14 +1,15 @@
-from fastapi import APIRouter, HTTPException, status, Depends
-from app.schemas.user import UserCreate  # ← asegúrate que apunta al schema correcto
+from fastapi import APIRouter, HTTPException, status
+from app.schemas.user import UserCreate
 from app.utils.auth import hash_password
-from app.database import get_db
-from pymongo.database import Database
+from app.database import user_collection
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 @router.post("/signup")
-def signup(user: UserCreate, db: Database = Depends(get_db)):
-    existing = db.users.find_one({"$or": [{"username": user.username}, {"email": user.email}]})
+async def signup(user: UserCreate):
+    existing = await user_collection.find_one({
+        "$or": [{"username": user.username}, {"email": user.email}]
+    })
     if existing:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -18,5 +19,6 @@ def signup(user: UserCreate, db: Database = Depends(get_db)):
     hashed_pw = hash_password(user.password)
     user_data = user.dict()
     user_data["password"] = hashed_pw
-    db.users.insert_one(user_data)
+
+    await user_collection.insert_one(user_data)
     return {"message": "Usuario registrado correctamente"}
